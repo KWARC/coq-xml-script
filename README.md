e62adde854e790fd146d488c21bb618b950c2e6e
Tom Wiesing <tkw01536@gmail.com>
2019-02-07 13:26:16 +0100

    New script

--- README.md
+++
@@ -1,7 +0,0 @@
-# coq-xml-script
-Script to run coq xml export
-
-## Libraries (TODO)
-
-- https://github.com/coq/bignums
-- https://github.com/coq-community/math-classes (depends on above)
---
+++ export_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# TODO befor running the script:
+#  opam repo add coq-released https://coq.inria.fr/opam/released
+# in the coq directory
+#  opam pin -n .
+
+# BUG? make clean required in the coq directory
+
+# REMEMBER TO CLEAN THE XML DESTINATION DIRECTORY RECURSIVELY
+#  - how to do that safely in the script?
+
+XML_ROOT="$(pwd)/xml"
+PACKAGES=$(opam list -a --repos coq-released --short -S | grep -v coqide)
+for i in $PACKAGES
+do
+        export COQ_XML_LIBRARY_ROOT="$XML_ROOT/$i"
+        mkdir "$COQ_XML_LIBRARY_ROOT"
+        echo "Exporting $i to $COQ_XML_LIBRARY_ROOT"
+        opam uninstall "$i" --yes > /dev/null
+        opam install "$i" > "$COQ_XML_LIBRARY_ROOT/opam.log" 2>&1 || touch "$COQ_XML_LIBRARY_ROOT/ABORTED"
+done
